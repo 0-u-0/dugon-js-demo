fd977fa77b4e4dc8594db3cda8ba11a60d8f11f5
Chen Cong <craterone11@gmail.com>
2020-04-11 20:05:38 +0900

    update sdk

--- dugon.js
+++ dugon.js
@@ -1752,6 +1752,15 @@
 	    };
 	  }
 
+	  checkSender(senderId){
+	    for(let sender of this.senders){
+	      if( (senderId === sender.senderId) && sender.available){
+	        return sender
+	      }
+	    }
+	    return null;
+	  }
+
 	  getLocalSdpData(sender, localSdp) {
 	    let localSdpObj = lib.parse(localSdp.sdp);
 
@@ -1827,13 +1836,15 @@
 	}
 
 	class Receiver {
-	  constructor(senderId, tokenId, receiverId, kind, rtpParameters, metadata) {
+	  constructor(senderId, tokenId, receiverId, kind, rtpParameters, senderPaused, metadata) {
 	    this.senderId = senderId;
 	    this.tokenId = tokenId;
 	    this.receiverId = receiverId;
 	    this.rtpParameters = rtpParameters;
 
 	    this.kind = kind;
+
+	    this.senderPaused = senderPaused;
 
 	    this.metadata = metadata;
 
@@ -1865,8 +1876,8 @@
 	    }
 	  }
 
-	  addReceiver(senderId, tokenId, receiverId, kind, rtpParameters, metadata) {
-	    const receiver = new Receiver(senderId, tokenId, receiverId, kind, rtpParameters, metadata);
+	  addReceiver(senderId, tokenId, receiverId, kind, rtpParameters, senderPaused, metadata) {
+	    const receiver = new Receiver(senderId, tokenId, receiverId, kind, rtpParameters, senderPaused, metadata);
 	    receiver.mid = String(this.currentMid++);
 	    this.receivers.set(senderId, receiver);
 	    return receiver;
@@ -1899,8 +1910,7 @@
 	    receiver.transceiver = transceiver;
 	    this.ontrack(transceiver.receiver.track, receiver);
 
-	    //TODO: consumer resume
-
+	    //TODO: receiver resume
 	    if (!this.isGotDtls) {
 	      this.isGotDtls = true;
 	      //dtls
@@ -2104,13 +2114,48 @@
 
 	  async pause(senderId) {
 	    let transportId = null;
-	    if(this.subscriber.receivers.get(senderId)){
-	      transportId = this.subscriber.transportId;
+	    let role = null;
+	    if (this.subscriber && this.subscriber.receivers.get(senderId)) {
+	      transportId = this.subscriber.id;
+	      role = 'sub';
+	    } else if (this.publisher && this.publisher.checkSender(senderId)) {
+	      transportId = this.publisher.id;
+	      role = 'pub';
+	    }
+
+	    if (transportId) {
+	      this.socket.request({
+	        event: 'pause',
+	        data: {
+	          transportId,
+	          senderId,
+	          role
+	        }
+	      });
 	    }
 	  }
 
-	  async resume() {
+	  async resume(senderId) {
+	    let transportId = null;
+	    let role = null;
+	    if (this.subscriber && this.subscriber.receivers.get(senderId)) {
+	      transportId = this.subscriber.id;
+	      role = 'sub';
+	    } else if (this.publisher && this.publisher.checkSender(senderId)) {
+	      transportId = this.publisher.id;
+	      role = 'pub';
+	    }
 
+	    if (transportId) {
+	      this.socket.request({
+	        event: 'resume',
+	        data: {
+	          transportId,
+	          senderId,
+	          role
+	        }
+	      });
+	    }
 	  }
 
 
@@ -2131,14 +2176,35 @@
 	        this.onout(tokenId);
 	        break;
 	      }      case 'publish': {
-	        let { senderId, tokenId, metadata, kind, rtpParameters, receiverId } = data;
-	        let receiver = this.subscriber.addReceiver(senderId, tokenId, receiverId, kind, rtpParameters, metadata);
-	        this.onreceiver(receiver, tokenId, senderId, metadata);
+	        let { senderId, tokenId, metadata, kind, rtpParameters, receiverId, senderPaused } = data;
+	        if (this.subscriber) {
+	          let receiver = this.subscriber.addReceiver(senderId, tokenId, receiverId, kind, rtpParameters, senderPaused, metadata);
+	          this.onreceiver(receiver, tokenId, senderId, metadata);
+	        }
+
 	        break;
 	      }      case 'unpublish': {
 	        let { senderId, tokenId } = data;
-	        this.subscriber.removeReceiver(senderId);
+	        if(this.subscriber){
+	          this.subscriber.removeReceiver(senderId);
+	        }
 
+	        break;
+	      }
+	      case 'pause': {
+	        let { senderId } = data;
+	        if(this.subscriber){
+	          this.subscriber.removeReceiver(senderId);
+	        }
+
+	        break;
+	      }
+	      case 'resume': {
+	        let { senderId } = data;
+	        if(this.subscriber){
+	          this.subscriber.removeReceiver(senderId);
+	        }
+	        
 	        break;
 	      }
 	      default: {
